5d3475aa2550a9cf6e3e64580e929d39b3ada69b
pratyaksh137 <pratyaksh1032001@gmail.com>
2023-10-23 16:48:17 +0530

    one to one in mongoose

--- onetoone.js
+++ onetoone.js
@@ -1,1 +1,34 @@
+//Below is an example for defining a basic One to one relationships b/w two collections in mongoose 
 const express = require('express');
+const app =  express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://0.0.0.0:27017/testRelations');
+
+const ownerSchema = new mongoose.Schema({
+    name:String
+});
+
+const houseSchema = new mongoose.Schema({
+    area:String,
+    city:String,
+    pincode:Number,
+    owner: {type: mongoose.Schema.Types.ObjectId, ref:'Owner'} //relation is defined here.
+});
+
+const Owner = mongoose.model('Owner', ownerSchema);
+const House = mongoose.model('House', houseSchema);
+
+const owner = new Owner({name:'Pratyaksh'});
+owner.save();
+
+const house = new House({
+    area:'Niranjanpur',
+    city:'Dehradun',
+    pincode:248001,
+    owner:owner._id
+});
+house.save();
+
+
