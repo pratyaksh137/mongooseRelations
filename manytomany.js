757b1a587bc7b58138d4a6980249994ba278f354
pratyaksh137 <pratyaksh1032001@gmail.com>
2023-10-23 18:19:13 +0530

    Many to many not completed yet

---
+++ manytomany.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://0.0.0.0:27017/testRelations');
+
+const ownerSchema = new mongoose.Schema({
+    name: String
+}, {
+    collection: 'owners'
+});
+
+const houseSchema = new mongoose.Schema({
+    city: String,
+    pincode: Number
+}, { collection: 'houses' });
+
+const houseowner = new mongoose.Schema({
+    houseOwner: { type: mongoose.Schema.Types, ref: 'Owner' },
+    ownerHouse: { type: mongoose.Schema.Types, ref: 'House' }
+});
+
+//data add krna 
+
